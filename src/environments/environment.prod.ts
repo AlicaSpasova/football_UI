0f3c0402f287c971bbdf2128c963728de3e5d9ec
Alica <kats@plazus.com>
2023-11-30 12:59:02 +0100

    Auto-generated commit [ci skip]

--- src/environments/environment.prod.ts
+++
@@ -1,6 +0,0 @@
-export const environment = {
-  production: true,
-  xRapidapiHost: "v3.football.api-sports.io",
-  xRapidapiKey: "cd5f3b9957d553f4fb0130448a9f5b83",
-  url: "https://v3.football.api-sports.io"
-};
